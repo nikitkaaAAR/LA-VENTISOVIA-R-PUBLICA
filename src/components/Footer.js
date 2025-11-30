ec7681d38a9d292175d32901eb6c0797d8eae0ad
nikitkaaAAR <nikita2009guz@gmail.com>
2025-11-30 22:15:44 +0200

    Load React from CDN modules directly

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from '../react.js';
+
+const Footer = () =>
+  React.createElement(
+    'footer',
+    { className: 'footer' },
+    React.createElement('div', { className: 'footer__logo' }, 'Госуслуги'),
+    React.createElement(
+      'div',
+      { className: 'footer__links' },
+      ['О проекте', 'Безопасность', 'Поддержка', 'Мобильное приложение'].map((item) =>
+        React.createElement('a', { key: item, href: '#', className: 'link' }, item)
+      )
+    ),
+    React.createElement('p', { className: 'footer__meta' }, 'Работаем для людей. 2025 © Госуслуги')
+  );
+
+export default Footer;
---
+++ src/react-dom-client.js
@@ -0,0 +1,2 @@
+export * from 'https://esm.sh/react-dom@18.3.1/client';
+export { default } from 'https://esm.sh/react-dom@18.3.1/client';
