99df7a02781d0ac7d54ae0935cee0ba2bcbbdf9d
nikitkaaAAR <nikita2009guz@gmail.com>
2025-11-30 21:50:43 +0200

    Handle storage clears for synced state

---
+++ main.js
@@ -0,0 +1,5 @@
+import App from './src/App.js';
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+root.render(React.createElement(App));
---
+++ src/components/ServiceGrid.js
@@ -0,0 +1,43 @@
+const ServiceCard = ({ title, description, action, onOpen }) =>
+  React.createElement(
+    'article',
+    { className: 'card' },
+    React.createElement('h3', null, title),
+    React.createElement('p', null, description),
+    React.createElement('div', { className: 'card__actions' },
+      React.createElement('button', { className: 'btn btn-secondary', onClick: onOpen }, action),
+      React.createElement('button', { className: 'btn btn-ghost', onClick: onOpen }, 'Подробнее')
+    )
+  );
+
+const ServiceGrid = ({ services, onSelectService }) =>
+  React.createElement(
+    'section',
+    { className: 'services', id: 'all-services' },
+    React.createElement(
+      'div',
+      { className: 'section__header' },
+      React.createElement(
+        'div',
+        null,
+        React.createElement('p', { className: 'section__eyebrow' }, 'Популярные сервисы'),
+        React.createElement('h2', null, 'Выберите нужную услугу')
+      ),
+      React.createElement('a', { href: '#all-services', className: 'link' }, 'Смотреть все')
+    ),
+    React.createElement(
+      'div',
+      { className: 'services__grid' },
+      services.length
+        ? services.map((service) =>
+            React.createElement(ServiceCard, { key: service.id, ...service, onOpen: () => onSelectService(service.id) })
+          )
+        : React.createElement(
+            'p',
+            { className: 'services__empty' },
+            'Услуги по запросу не найдены. Попробуйте другой поиск или выберите тег выше.'
+          )
+    )
+  );
+
+export default ServiceGrid;
